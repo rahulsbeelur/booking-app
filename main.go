a0c061ae255b83cb8e52448f793611ff79ab776b
Rahul S Beelur <rahul.s.beelur@lowes.com>
2024-05-11 21:14:19 +0530

    Created functions

--- main.go
+++ main.go
@@ -16,29 +16,13 @@ func main() {
 	fmt.Printf("We have total %v out of which %v are available \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here")
 
-
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
 	bookingList := []string{}
 
 	for {
-		fmt.Println("Enter your first name")
-		fmt.Scan(&firstName)
-
-		fmt.Println("Enter your last name")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Enter your email address")
-		fmt.Scan(&email)
-
-		fmt.Println("Enter number of tickets you want to buy")
-		fmt.Scan(&userTickets)
-
+		firstName, lastName, email, userTickets := getUserInput()
 		if remainingTickets < userTickets {
 			fmt.Printf("We only have %v tickets left, we cannot book %v tickets for you\n", remainingTickets, userTickets)
-			continue
+			return
 		}
 
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive confirmation on - %v \n", firstName, lastName, userTickets, email)
@@ -49,12 +33,8 @@ func main() {
 
 		fmt.Printf("We have %v tickets left for the conference", remainingTickets)
 
-		firstNames := []string{}
-		for _, booking := range bookingList {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("Booking list first names - %v \n", firstNames)
+		firstNames := printFirstNames(bookingList)
+		fmt.Printf("\nBooking list first names - %v \n", firstNames)
 
 		noTicketsLeft := remainingTickets == 0
 		if noTicketsLeft {
@@ -63,4 +43,34 @@ func main() {
 		}
 	}
 
+}
+
+func printFirstNames(bookingList []string) []string {
+	firstNames := []string{}
+	for _, booking := range bookingList {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames;
+}
+
+func getUserInput() (string, string, string, int) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets you want to buy")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
 }
